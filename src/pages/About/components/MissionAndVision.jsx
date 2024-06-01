7afe969b9bbe536161597ea61fc74ef02053c600
lmd <lmdiaz1993@gmail.com>
2024-06-01 16:54:03 -0300

    feat: se agregaron las secciones con información en texto de la empresa

--- src/pages/About/components/MissionAndVision.jsx
+++ src/pages/About/components/MissionAndVision.jsx
@@ -1,22 +1,20 @@
 import styles from "../about.module.css"
-const {seccion_centrada,contenido_centrado}=styles
+const {section_centered,container_centered}=styles
 
 function MissionAndVision() {
     
         return (
-          <section className={seccion_centrada}>
-            <div className={contenido_centrado}>
+          <section className={section_centered}>
+            <div className={container_centered}>
               <h2>Misión y Visión</h2>
-              <article>
+              <div>
                 <p>
-                  Nuestra misión en **BOOKINGIDW** es inspirar la exploración, fomentar la diversidad cultural y promover la comprensión global a través de la hospitalidad compartida. Nos comprometemos a proporcionar una plataforma confiable y segura donde las personas puedan descubrir y reservar alojamientos únicos en todo el mundo, mientras que los anfitriones pueden compartir sus espacios de manera significativa y rentable. Estamos dedicados a crear un impacto positivo en las comunidades locales, promoviendo la inclusión y la sostenibilidad en cada viaje.
+                  Nuestra misión en <strong>BOOKINGIDW</strong> es inspirar la exploración, fomentar la diversidad cultural y promover la comprensión global a través de la hospitalidad compartida. Nos comprometemos a proporcionar una plataforma confiable y segura donde las personas puedan descubrir y reservar alojamientos únicos en todo el mundo, mientras que los anfitriones pueden compartir sus espacios de manera significativa y rentable. Estamos dedicados a crear un impacto positivo en las comunidades locales, promoviendo la inclusión y la sostenibilidad en cada viaje.
                 </p>
-              </article>
-              <article>
                 <p>
                   Nos visualizamos como líderes mundiales en la creación de experiencias únicas y significativas a través de la conexión humana. Buscamos transformar la manera en que las personas viajan, exploran y se conectan con el mundo, ofreciendo alojamientos excepcionales y experiencias auténticas que enriquecen la vida de nuestros huéspedes y anfitriones.
                 </p>
-              </article>
+                </div>
             </div>
           </section>
         );
--- src/pages/About/components/Team.jsx
+++ src/pages/About/components/Team.jsx
@@ -1,4 +1,22 @@
+import styles from "../about.module.css"
+const {section_centered,container_centered}=styles
+
 function Team() {
-    return <p>TEXTO TEAM</p>
+    return ( <section className={section_centered}>
+    <div className={container_centered}>
+    <h2>Nuestro Equipo</h2>
+    <p>
+      Son el corazón de nuestra empresa. Compuesto por personas apasionadas
+      y diversas de diferentes partes del mundo, trabajamos juntos para
+      hacer realidad nuestra visión y misión. Desde nuestro equipo de
+      desarrollo tecnológico que impulsa la innovación hasta nuestro equipo
+      de atención al cliente que garantiza experiencias excepcionales, cada
+      miembro desempeña un papel vital en el éxito de nuestra empresa. Nos
+      enorgullece nuestra cultura colaborativa, inclusiva y orientada al
+      impacto, y nos esforzamos por fomentar un entorno donde cada individuo
+      pueda crecer, aprender y prosperar.
+    </p>
+    </div>
+  </section>)
 }
 export default Team
