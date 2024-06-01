6ccc18393885f7197ecbc00f96b4c647055a01bb
lmd <lmdiaz1993@gmail.com>
2024-06-01 16:52:00 -0300

    feat: Se agrego el componente de visualizar imagenes

--- src/pages/About/components/ImageView.jsx
+++ src/pages/About/components/ImageView.jsx
@@ -1,4 +1,15 @@
-function ImageView() {
-    return <p>Image</p>
+import PropTypes from 'prop-types';
+import styles from "../about.module.css";
+
+const {image_container}=styles
+
+function ImageView({legend,placeImage}) {
+    return (<div className={image_container}>
+        <figure><img src= {placeImage} alt="team"/>
+        <legend>{legend} </legend></figure>
+    </div>)
 }
+ImageView.propTypes = {
+    legend: PropTypes.string, placeImage: PropTypes.string,
+  };
 export default ImageView
