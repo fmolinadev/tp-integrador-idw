2594215ef3c9395adefb652842584ed735bc17bb
fmolinadev <franciscomolina.dev@gmail.com>
2024-06-02 21:32:49 -0300

    feat: agregue configuracion base de endpoints para las solicitudes

---
+++ src/global.request.js
@@ -0,0 +1,5 @@
+export const BASE_URL = {
+  LOCAL: "http://localhost:3001",
+  ALOJAMIENTOS: "http://localhost:3001/alojamiento",
+  TIPO_ALOJAMIENTOS: "http://localhost:3001/tiposAlojamiento",
+};
