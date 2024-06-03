2df9ee7f635f9a4c3d6f6ce0b61f73f5f53e3ed8
fmolinadev <franciscomolina.dev@gmail.com>
2024-06-02 21:30:30 -0300

    feat: actualice estilo en el layout

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { Footer, Header } from "../components";
-
+import styles from "./layout.module.css";
 function DefaultLayout() {
   return (
-    <>
+    <div className={styles.body_container}>
       <Header />
       <Outlet />
       <Footer />
-    </>
+    </div>
   );
 }
 
